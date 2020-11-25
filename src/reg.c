99cae3c1c2ecd6477d1b0d1ad849c796f6bd1089
Simon Urbanek <simon.urbanek@R-project.org>
2020-11-25 15:41:36 +1300

    add C-level API registration

---
+++ src/reg.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <R_ext/Rdynload.h>
+#include <Rinternals.h>
+
+/* read.c */
+extern SEXP read_tiff(SEXP sFn, SEXP sNative, SEXP sAll, SEXP sConvert, SEXP sInfo, SEXP sIndexed, SEXP sOriginal);
+/* write.c */
+extern SEXP write_tiff(SEXP image, SEXP where, SEXP sBPS, SEXP sCompr, SEXP sReduce);
+
+static const R_CallMethodDef CAPI[] = {
+    {"read_tiff",  (DL_FUNC) &read_tiff , 7},
+    {"write_tiff", (DL_FUNC) &write_tiff, 5},
+    {NULL, NULL, 0}
+};
+
+void R_init_tiff(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CAPI, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
