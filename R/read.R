4c3d10d7d20bc4c545e7061c9f0f66e6894217e9
Simon Urbanek <simon.urbanek@R-project.org>
2021-03-26 09:28:36 +1300

    make sure the result of readTIFF(.., all=TRUE, payload=FALSE) is a data frame

--- R/read.R
+++ R/read.R
@@ -10,14 +10,20 @@ readTIFF <- function(source, native=FALSE, all=FALSE, convert=FALSE, info=FALSE,
        if (is.integer(x))
            as.data.frame(attributes(x), stringsAsFactors=FALSE)
        else {
+           ## fetch tag from each image
            x <- lapply(x, attributes)
+           ## construct a union of all tags
            u <- unique(unlist(lapply(x, names)))
-           do.call("rbind", lapply(x, function(o) {
+           ## match tags from each image using NA for missing
+           d <- do.call("rbind.data.frame", lapply(x, function(o) {
                o <- c(o, list(`NA`=NA))
                d <- o[match(u, names(o), length(o))]
                names(d)=u
                d
            }))
+           ## clear row names so they are sequential
+           row.names(d) <- NULL
+           d
        }
     }
 }
